284c026060ea0370af6b7cc1cc59b0f149b9dcff
Davi Ferreira <daviferreirart@gmail.com>
2021-03-06 20:08:59 -0300

    alguns mudanças na url da requisição

--- trabalho_vp1.py
+++ trabalho_vp1.py
@@ -2,21 +2,21 @@ import socket
 
 
 def main():
-    requisicao = b"GET /graduacao/sistemas-de-informacao/ HTTP/1.1\nHost: https://www.uni7.edu.br \n\n"
+    requisicao = b'GET /graduacao/ HTTP/1.1\nHost: http://www.uni7.edu.br \n\n'
 
     socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    socketTCP.connect(("uni7.edu.br", 80))
-    
-    print("Conexão iniciada!")
-    
+    socketTCP.connect((b"uni7.edu.br", 80))
+
+
     socketTCP.sendall(requisicao)
 
-    resultado = socketTCP.recv(10000)
+    resultado = socketTCP.recv(1024)
 
     while (len(resultado) > 0):
+        print("Conexão iniciada!")
         print(resultado)
-        resultado = socketTCP.recv(10000)
+        resultado = socketTCP.recv(1024)
 
     socketTCP.close()
 
